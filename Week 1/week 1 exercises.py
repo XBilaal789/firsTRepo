4c8057770117b70555f747a8c2213aa642fa465a
XBilaal789 <bilaalahmed619@gmail.com>
2023-10-28 01:35:47 +0100

    Week 1 exercise completed

---
+++ Week 1/week 1 exercises.py
@@ -0,0 +1,40 @@
+print("the program has executed")
+print("Hello, how are you")
+print("The weather today is very nice")
+print("I am very hungry today")
+help("keywords")
+help("print")
+help("input")
+print("hello")
+sum= 25+20
+print (sum)
+sum= 10+20-15
+print (sum)
+sum= 10*5
+print (sum)
+sum= 100/33
+print (sum)
+sum= 100//33
+print (sum)
+sum= 100**2
+print (sum)
+sum= 10%3
+print (sum)
+sum= 100+5*2
+print (sum)
+sum= 100-5*10+5
+print (sum)
+sum= 5*10**2
+print (sum)
+sum= (10+5)*2
+print (sum)
+sum= 10-5*(10+5)
+print (sum)
+sum= (5*10)**2
+print (sum)
+sum= 12+(5*2+3)
+print (sum)
+sum= 12+(5*(2+3))
+print (sum)
+
+
