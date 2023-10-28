8697ced2db6ee78a9b5d799a26f53f6d1f02a7c2
XBilaal789 <bilaalahmed619@gmail.com>
2023-10-28 03:18:36 +0100

    Week 2 Practical 50% completed

---
+++ Week 2/week 2 practical.py
@@ -0,0 +1,37 @@
+total = 100
+print("The total is", total)
+total = total + 99
+print("The total is now", total)
+total -= 1
+print("The total is", total)
+total *= 4
+print("The total is", total)
+total /= 2
+print("The total is", total)
+total = 98.2
+code = 5
+total /= 5
+print("the average is", total)
+value = False
+value_type = type(value)
+print(value_type)
+value = 1000
+value_type = type(value)
+print(value_type)
+value = 100.111
+value_type = type(value)
+print(value_type)
+value = "Hello"
+value_type = type(value)
+print(value_type)
+value = True
+value_type = type(value)
+print(value_type)
+value = 100/5
+value_type = type(value)
+print(value_type)
+value = 100//5
+value_type = type(value)
+print(value_type)
+print(10 + 10)
+print("10" + "10")
