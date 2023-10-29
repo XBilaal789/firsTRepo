5b5555275415abdcf82cf6b7d054d2935a1e27d6
XBilaal789 <bilaalahmed619@gmail.com>
2023-10-29 02:24:29 +0000

    Week 3 Practical 10% completed

---
+++ Week 3/Week 3 practical.py
@@ -0,0 +1,15 @@
+print(10 < 100)
+print(100 != 100)
+print(5 >= 50)
+
+age = 18
+print(age > 13)
+print(age < 27)
+print(age > 35)
+
+print("a" < "b")
+print("b" < "a")
+print("John" < "Terry")
+print("new")
+
+print("john" < "Terry")
